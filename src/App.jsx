ff84e3a93ea0ab697e03c42a19f96ccf7195ef7d
Tom Ogasawara <tom.ogasawara@gmail.com>
2017-02-16 12:52:56 -0800

    Begin first component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2017'
+    };
+  }
+
+  changeDeadline() {
+    this.setState({deadline: 'New deadline'});
+  }
+
   render() {
     return(
-      <div> test 123</div>
+      <div className='App'>
+        <div className='App-title'>Countdown to { this.state.deadline }</div>
+        <div>
+          <div className='clock-days'>1</div>
+          <div className='clock-hours'>1</div>
+          <div className='clock-minutes'>1</div>
+          <div className='clock-seconds'>1</div>
+
+        </div>
+        <input placeholder='new date' />
+        <button onClick={ () => this.changeDeadline() }>Submit</button>
+      </div>
     );
   }
 }
